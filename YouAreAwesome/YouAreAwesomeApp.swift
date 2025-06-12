2ffd92e8568b5ef30b34171a63f10e9f66f099fe
Jamie Chen <jamie.codeapp@gmail.com>
2025-06-12 13:57:05 +0800

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Jamie on 2025/6/12.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
